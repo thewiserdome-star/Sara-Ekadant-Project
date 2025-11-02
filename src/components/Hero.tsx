cf09cc0560e5606d3fe85b9efdad55c203534fac
thewiserdome-star <thewiserdome@gmail.com>
2025-11-02 17:29:49 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export function Hero({ onScrollToGallery }: HeroProps) {
           <img
               src="https://saraekadant.blob.core.windows.net/mediasaraekadant/001_logo.JPG"
               alt="Logo"
-              className="w-24 h-24"
+              className="w-50 h-50"
               style={{ objectFit: 'contain' }}
             />
         </div>
