2295e1e0a851e51fbf7d4f6b693150ac85cde23c
thewiserdome-star <thewiserdome@gmail.com>
2025-11-10 17:22:08 +0530

    Update ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -18,6 +18,21 @@ const images = [
     url: 'https://saraekadant.blob.core.windows.net/mediasaraekadant/13_05_building_side copy.jpg',
     title: 'Horizontal Side Elevation',
   },
+ {
+    url: 'https://saraekadant.blob.core.windows.net/mediasaraekadant/13_04_building_perspective copy.jpg',
+    title: 'Building Perspective',
+  },
+  {
+    url: 'https://saraekadant.blob.core.windows.net/mediasaraekadant/13_07 copy.jpg',
+    title: 'Left Diagonal Side Elevation',
+  },
+  {
+    url: 'https://saraekadant.blob.core.windows.net/mediasaraekadant/13_04_Night_view copy.jpg',
+    title: 'Night View Elevation',
+  },
+
+
+  
 ];
 
 export function ImageGallery() {
