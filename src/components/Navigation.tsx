46849940138b4682b9debd2242e7a3c7281197f3
thewiserdome-star <thewiserdome@gmail.com>
2025-11-16 18:04:05 +0530

    Update logo image source in Navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -82,7 +82,7 @@ export function Navigation() {
         <div className="flex items-center justify-between h-20">
           <div className="flex items-center gap-3">
             <img
-              src="https://saraekadant.blob.core.windows.net/mediasaraekadant/001_logo.JPG"
+              src="https://saraekadant.blob.core.windows.net/mediasaraekadant/SaraCorpLogoShort.jpg"
               alt="Sara Ekadant logo"
               className="w-12 h-12"
               style={{ objectFit: 'contain' }}
